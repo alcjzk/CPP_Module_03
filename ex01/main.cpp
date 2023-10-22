8598bcc7f30c4d8e19b2b65c3d9d1c9df774dfbb
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-10-22 22:31:53 +0300

    Ex01 and tests

---
+++ ex01/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main(void)
+{
+    std::cout << "\n-- Default constructors:\n";
+    ClapTrap unnamed;
+    ClapTrap foo("Foo");
+
+    std::cout << "\n-- Copy constructor\n";
+    ClapTrap bar(foo);
+
+    std::cout << "\n-- Attack\n";
+    for (int i = 0; i < 11; i++) 
+    {
+        std::cout << i << ": foo.attack('Baz'): ";
+        foo.attack("Baz");
+    }
+    std::cout << "Claptraps cannot attack without energy\n";
+
+    std::cout << "\nfoo = bar: ";
+    foo = bar;
+
+    std::cout << "\n-- Take damage\n";
+    std::cout << "1: foo.takeDamage(6): ";
+    foo.takeDamage(6);
+    std::cout << "2: foo.takeDamage(6) (only has 4 hitpoints left so damage is reduced):\n\t";
+    foo.takeDamage(6);
+    std::cout << "3: foo.takeDamage(6): ";
+    foo.takeDamage(6);
+    std::cout << "Dead claptraps don't take damage\n";
+
+    std::cout << "\nfoo = bar: ";
+    foo = bar;
+
+    std::cout << "\n-- Self repair\n";
+    std::cout << "0: foo.beRepaired(UINT_MAX - 100): ";
+    foo.beRepaired(std::numeric_limits<unsigned int>::max() - 100);
+    std::cout << "1: foo.beRepaired(UINT_MAX): (reduced repair due to numeric limits)\n\t";
+    foo.beRepaired(std::numeric_limits<unsigned int>::max());
+    for (int i = 2; i < 10; i++)
+    {
+        std::cout << i << " : foo.beRepaired(10): ";
+        foo.beRepaired(10);
+    }
+    std::cout << "10: foo.beRepaired(10): Claptraps with no energy cannot heal\n";
+    foo.beRepaired(10);
+
+    std::cout << "\n--ScavTrap constructors\n";
+    ScavTrap unnamed_st;
+    ScavTrap baz("Baz");
+
+    std::cout << "\nbaz.guardGate() : ";
+    baz.guardGate();
+
+    std::cout << "\n-- Returning from main\n";
+    return EXIT_SUCCESS;
+}
